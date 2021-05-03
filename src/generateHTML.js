8abb55a4c8468c86358f843b747e789ed21918cd
Candice Whiskey <candicewhiskey77@gmail.com>
2021-05-02 20:16:11 -0400

    updating files

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,4 +1,4 @@
-const generateManager = functin (manager){
+const generateManager = function (manager){
     return
     <div class ="col-4 mt-4">
         <div class="card h-100">
@@ -11,5 +11,21 @@ const generateManager = functin (manager){
             <p class="id">ID: ${manager.id}</p>
             <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
             <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+</div>
+
+const generateManager = function (engineer){
+    return
+    <div class ="col-4 mt-4">
+        <div class="card h-100">
+        <div class="card-header">
+            <h3>${engineer.name}</h3>
+            <h4>Engineer</h4><i class="material-icons">contect_paste</i>
+            </div>
+
+            <div class="card-body">
+            <p class="id">ID: ${engineer.id}</p>
+            <p class="email">Email: <a href="mailto:${manager.email}">${engineer.email}</a></p>
+            <p class="office">Office Number: ${engineer.officeNumber}</p>
             </div>
 </div>
